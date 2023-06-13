028981de9365f66fbc78647bee7de33a40dc6024
FeliciaTorwald <felicia.torwald@gmail.com>
2023-06-13 12:37:53 +0200

    Real time clock :clock1:

---
+++ Tamagotchi/Assets/Scripts/Clock.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    public TMP_Text clockText;
+
+    private void Awake()
+    {
+        DateTime time = DateTime.Now;
+        string hour = LeadingZero(time.Hour);
+        string minute = LeadingZero(time.Minute);
+
+        clockText.text = hour + ":" + minute;
+    }
+
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
