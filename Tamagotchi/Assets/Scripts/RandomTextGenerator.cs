91b0fd3a247b9ae122e6cd6ca6cf286fa9a1380f
FeliciaTorwald <felicia.torwald@gmail.com>
2023-06-22 21:27:15 +0200

    messages function and graphics

---
+++ Tamagotchi/Assets/Scripts/RandomTextGenerator.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class RandomTextGenerator : MonoBehaviour
+{
+    public TextMeshProUGUI prettyText;
+    string[] messages = { "you look cute today", "the sun doesnt even outdo your brightness", "venus blessed you I see","everyday I gaze upon you", "eternity doesnt seem to be that long with you",
+        "you spark like a thunderstorm","is this a dream?","you're sweet like sugar","with you awake this will be a good day","who am I? your secret admirer", "luminous like the moon",
+        "you're finally awake","like a flower I hope you're ready to get picked","you smell like a flowerfield of roses","was that you falling or a star?"};
+    [SerializeField] private bool[] beenRead;
+    [SerializeField] private float textRead;
+    private int counter;
+    string randomText;
+
+    void Start()
+    {
+        beenRead = new bool[messages.Length];
+    }
+    public void GenerateRandomText()
+    {
+        randomText = messages[Random.Range(0, messages.Length)];
+            prettyText.text = randomText;
+        //textRead++;
+        //beenRead[int.Parse(randomText)] = true;
+
+        ////prettyText.text = randomText;
+
+        //while (beenRead[int.Parse(randomText)])
+        //{
+        //    //randomText = messages[Random.Range(0, messages.Length)];
+        //    counter++;
+        //    if (counter >= 100)
+        //    {
+        //        ResetShuffle();
+        //    }
+        //}
+    }
+
+    public void ResetShuffle()
+    {
+        textRead = 0;
+        for (int i = 0; i < messages.Length; i++)
+        {
+            if (i == messages.Length)
+                break;
+            else
+                beenRead[i] = false;
+        }
+    }
+}
